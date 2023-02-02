ecbe99946c996a30c5cb8fe8fb7428216465fd96
EthanG78 <ethangarnier78@gmail.com>
2023-02-02 16:34:00 -0400

    cv_performance_test finds the brightest pixel in the image using minMaxLoc and draws a circle around it.

--- cv_performance_test/cv_performance_test/main.cpp
+++ cv_performance_test/cv_performance_test/main.cpp
@@ -9,9 +9,35 @@
 #include <chrono>
 #include "opencv2/opencv.hpp"
 
-void find_brightest_spot(cv::Mat oldImg, cv::Mat& newImg)
+// Ideas:
+// 1. Use minMaxLoc
+// 2. Use threshold
+void find_brightest_spot(cv::Mat &img, int radius)
 {
-	cv::GaussianBlur(oldImg, newImg, cv::Size(5, 5), 0);
+	if (img.empty()) return;
+
+	// Clone and convert the image to grayscale
+	cv::Mat proccImg = img.clone();
+	cv::cvtColor(img, proccImg, cv::COLOR_BGR2GRAY);
+
+	// Apply a Gaussian Blur to perform high frequency filtering
+	cv::Size blurSize = cv::Size(radius, radius);
+	cv::GaussianBlur(img, proccImg, blurSize, 0);
+
+	// We must split the image into a single channel array
+	cv::Mat channels[3];
+	cv::split(proccImg, channels);
+
+	// Determine the maximum in the image
+	cv::Point maxLoc;
+	cv::minMaxLoc(channels[0], NULL, NULL, NULL, &maxLoc);
+
+	// Draw a circle around the brightest location
+	cv::circle(img, maxLoc, radius * 10, cv::Scalar(255, 0, 0, 0));
+
+	// Draw a line from the center of the image to the brightest spot
+	cv::Point imgCenter = cv::Point(img.cols / 2, img.rows / 2);
+	cv::line(img, imgCenter, maxLoc, cv::Scalar(255, 0, 0, 0));
 }
 
 int main()
@@ -36,16 +62,14 @@ int main()
 		
 
 		// DO PROCESSING HERE
-		cv::Mat processedImg;
-		find_brightest_spot(frame, processedImg);
+		find_brightest_spot(frame, 5);
 
-		cv::imshow("frame capture", processedImg);
-
-		
 		// Stop timing and print result
 		auto stop = std::chrono::high_resolution_clock::now();
 		auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 		std::cout << "Processing Time: " << duration.count() << std::endl;
+
+		cv::imshow("frame capture", frame);
 
 		// Wait 16 ms (roughly 60FPS)
 		// before processing the next frame. If the user
